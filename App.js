3dcdd223116471a45458ee7f841aaf5a5772f0cb
Case104 <jpcase104@gmail.com>
2016-06-25 18:01:42 -0400

    Experimenting with props and file structure.

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
   render(){
-    return <div>Hi</div>
+  	let txt = this.props.txt
+    return <h1>{txt}</h1>
   }
 }
 
