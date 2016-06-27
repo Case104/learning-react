b8da0f0ed125339fe0b7a9832c37c6937278b0ff
Case104 <jpcase104@gmail.com>
2016-06-27 12:38:55 -0400

    Owner ownee relationship

--- App.js
+++ App.js
@@ -2,10 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+
+	constructor(){
+		super();
+		this.state = { txt: 'this is the state text'}
+		this.update = this.update.bind(this)
+	}
+
+	update(e){
+		this.setState({txt: e.target.value})
+	}
+
   render(){
-  	let txt = this.props.txt
-    return <h1>{txt}</h1>
+  	let txt = this.state.txt
+    return (
+    	<div>
+    		<Widget txt={txt} update={this.update} />
+    	</div>
+    );
   }
+}
+
+const Widget = (props) => {
+ return (
+		<div>
+			<input type='text'
+			onChange={props.update} />
+			<h1>{props.txt}</h1>
+		</div>
+	);
 }
 
 export default App
